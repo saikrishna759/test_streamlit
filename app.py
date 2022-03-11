dc63064f6f546f98beecbeb386b55745e9a6dd66
saikrishna759 <44312928+saikrishna759@users.noreply.github.com>
2022-03-11 13:06:37 +0530

    sdf

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Welcome to Streamlit!")
 
-st.write("Our first DataFram")
+st.write("Our first DataFrame")
 
 st.dataframe(
   pd.read_csv("https://raw.githubusercontent.com/saikrishna759/test_streamlit/main/combined_indian_songs_data.csv")
