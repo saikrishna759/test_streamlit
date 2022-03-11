e7282e31fba5ce0b87f839c46ea39cd488dd7a64
saikrishna759 <44312928+saikrishna759@users.noreply.github.com>
2022-03-11 13:03:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.title("Welcome to Streamlit!")
 st.write("Our first DataFram")
 
 st.dataframe(
-  pd.read_csv("./combined_indian_songs_data.csv")
+  pd.read_csv("combined_indian_songs_data.csv")
 )
